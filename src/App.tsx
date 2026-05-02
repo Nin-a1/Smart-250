140306ec5e5852afc888cf077a1ee5708b302f53
kantero-tech <hassandayisaba@gmail.com>
2026-05-02 12:48:42 +0200

    init: create smart-kigali-alert vite react-ts project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Box, Heading, Text, VStack } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <Box minH="100vh" p={8}>
+      <VStack align="start" gap={4}>
+        <Heading size="2xl">Smart Kigali Alert</Heading>
+        <Text>yes siiiiiiiiiiiiiir.</Text>
+      </VStack>
+    </Box>
+  )
+}
+
+export default App
